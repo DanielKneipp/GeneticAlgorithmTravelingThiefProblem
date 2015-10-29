9ef2a2792966e5a5a5bfefa40b2e13d31eae90a7
Daniel Kneipp <dkdsv@hotmail.com>
2015-10-29 00:13:07 -0200

    execBatch update.

--- execBatch.sh
+++ execBatch.sh
@@ -4,15 +4,13 @@ EXEC="./GA"
 NUM_EXECS=30
 GA_CONF="gaConf1.gaconf"
 REPORTS_PATH="reports/"
-STDCOUT_FILE="stdcout.log"
+STDCOUT_FILE="stdcout"
 PROBLEM_LIST="$(find . -name "*.ttp")"
-
-i=1;
 
 for ttp in ${PROBLEM_LIST}
 do
-  $EXEC $ttp $GA_CONF $NUM_EXECS $REPORTS_PATH $STDCOUT_FILE
-  $i=$i+1
+    ttp_name=$(basename $ttp)
+    echo $EXEC "$ttp $GA_CONF $NUM_EXECS $REPORTS_PATH ${STDCOUT_FILE}__${ttp_name}.log"
 done
 
-# GA instances/kroA100-ttp/kroA100_n990_uncorr_10.ttp  gaConf1.gaconf 30 reports/ stdcoutr.log
+# GA instances/kroA100-ttp/kroA100_n990_uncorr_10.ttp gaConf1.gaconf 30 reports/ stdcout__kroA100_n990_uncorr_10.ttp.log
